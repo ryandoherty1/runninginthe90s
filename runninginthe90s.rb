23c8a7655438f5c9ec34e89b7b729c1994b1e3ec
ryandoherty1 <37849271+ryandoherty1@users.noreply.github.com>
2020-01-17 15:43:27 -0800

    Add files via upload

---
+++ runninginthe90s.rb
@@ -0,0 +1,40 @@
+use_bpm 159
+use_synth :piano
+2.times do
+  play :E5
+  play :C5
+  sleep 0.59
+  play :E5
+  play :B5
+  sleep 0.59
+  play :F4
+  play :F2
+  sleep 0.59
+  play :E5
+  play :C5
+  sleep 0.59
+  play :E5
+  play :B5
+  sleep 0.59
+  play :F4
+  play :F2
+  sleep 0.7
+  play :E5
+  play :C5
+  sleep 0.59
+  play :E5
+  play :B5
+  sleep 0.59
+  play :D5
+  play :B5
+  play :F4
+  play :F2
+  sleep 0.7
+  play :D5
+  play :C5
+  sleep 0.59
+  
+end
+
+
+
